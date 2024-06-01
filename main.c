8ded36d15324b4dd7172f57856248c601b50cf0c
Travis Avey <travis.avey@pm.me>
2024-06-01 16:11:26 -0400

    very basic movement
    Need force. Need rotation. Need to 'wrap' back around

--- main.c
+++ main.c
@@ -1,20 +1,44 @@
 #include <raylib.h>
 
-const int width = 640;
-const int height = 480;
+const int width = 1000;
+const int height = 1000;
 
 int main(int argc, char **argv) {
 
   InitWindow(width, height, "Asteroids");
 
-  SetTargetFPS(60);
+  Vector2 shipNose = {(float)width / 2, (float)height / 2};
+  Vector2 shipLeft = {shipNose.x + 15, shipNose.y + 15};
+  Vector2 shipRight = {shipLeft.x - 30, shipLeft.y};
+
+  SetTargetFPS(120);
 
   while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_W)) {
+      shipNose.y -= 2;
+      shipLeft.y -= 2;
+      shipRight.y -= 2;
+    }
+    if (IsKeyDown(KEY_S)) {
+      shipNose.y += 2;
+      shipLeft.y += 2;
+      shipRight.y += 2;
+    }
+    if (IsKeyDown(KEY_A)) {
+      shipNose.x -= 2;
+      shipLeft.x -= 2;
+      shipRight.x -= 2;
+    }
+    if (IsKeyDown(KEY_D)) {
+      shipNose.x += 2;
+      shipLeft.x += 2;
+      shipRight.x += 2;
+    }
     BeginDrawing();
 
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
 
-    DrawText("Here is the asteroid game canvas", 190, 20, 20, LIGHTGRAY);
+    DrawTriangleLines(shipNose, shipLeft, shipRight, WHITE);
 
     EndDrawing();
   }
